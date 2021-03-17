20de6757777ed7a23363882efd060f4eaf8d84a5
Jamie Cansdale <jcansdale@github.com>
2021-03-17 16:51:01 +0000

    Place script inside async function

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const package = core.getInput('package');
-  const owner = core.getInput('owner');
-  const token = core.getInput('owner');
+async function run() {
+  try {
+    const package = core.getInput('package');
+    const owner = core.getInput('owner');
+    const token = core.getInput('owner');
 
-  const octokit = github.getOctokit(token);
-  const response = await octokit.request(`GET /${owner}/packages/container/${package}/versions`, { per_page: 100 });
-  for(version of response.data) {
-      if (version.metadata.container.tags.length == 0) {
-          console.log(`delete ${version.id}`)
-          const deleteResponse = await octokit.request(`DELETE /${owner}/packages/container/${package}/versions/${version.id}`, { });
-          console.log(`status ${deleteResponse.status}`)
-      }
+    const octokit = github.getOctokit(token);
+    const response = await octokit.request(`GET /${owner}/packages/container/${package}/versions`, { per_page: 100 });
+    for(version of response.data) {
+        if (version.metadata.container.tags.length == 0) {
+            console.log(`delete ${version.id}`)
+            const deleteResponse = await octokit.request(`DELETE /${owner}/packages/container/${package}/versions/${version.id}`, { });
+            console.log(`status ${deleteResponse.status}`)
+        }
+    }
+  } catch (error) {
+    core.setFailed(error.message);
   }
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
